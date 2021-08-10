ca51f7d7a9230856088c6da5e91d647d941cb154
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-08-10 12:04:29 -0700

    cleanup template, add new user PM

--- bot.js
+++ bot.js
@@ -1,44 +1,23 @@
-const {
-  AkairoClient,
-  CommandHandler,
-  InhibitorHandler,
-  ListenerHandler,
-} = require('discord-akairo');
-const owners = require('./owners.json');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const env = require('./utils/env-helper');
 require('dotenv').config({ path: require('find-config')('.env') });
 
-class MyClient extends AkairoClient {
-  constructor() {
-    super(
-      {
-        ownerID: owners,
-      },
-      {
-        disableMentions: 'everyone',
-      }
-    );
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-    this.commandHandler = new CommandHandler(this, {
-      directory: './commands/',
-      prefix: '?', // or ['?', '!']
-    });
+client.once('ready', () => {
+  console.log('Ready!');
+});
 
-    this.inhibitorHandler = new InhibitorHandler(this, {
-      directory: './inhibitors/',
-    });
-
-    this.listenerHandler = new ListenerHandler(this, {
-      directory: './listeners/',
-    });
-
-    this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
-    this.commandHandler.useListenerHandler(this.listenerHandler);
-    this.inhibitorHandler.loadAll();
-    this.listenerHandler.loadAll();
-    this.commandHandler.loadAll();
-  }
-}
-
-const client = new MyClient();
 client.login(env('TOKEN'));
+
+client.on('guildMemberAdd', (member) => {
+  client.users.fetch(member.id, false).then((user) => {
+    const newUserEmbed = new MessageEmbed()
+      .setColor('#00ffff')
+      .setTitle(`🙋‍♀️ Hello ${user.username}`)
+      .setDescription(
+        `You are now a member of a growing community of Strapi developers/users from all over the world who build awesome projects, and support each other.\n\n👋 We'd **love** to get to know you a bit better, so please **[fill out this short survey](https://strapi.typeform.com/to/TLM3Ae?channel=U01JT604ER2)** then introduce yourself in the [#introduction channel](https://discord.gg/2ZVmbQAuKW)! \n\nIf you're stuck for things to say, here are a few ideas: \n- Where you're from? \n- What tech stack do you use? \n- Your experience with Strapi? \n- What have you been working on?`
+      );
+    user.send({ embed: newUserEmbed });
+  });
+});
