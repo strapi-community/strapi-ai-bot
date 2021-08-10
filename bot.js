4513f214261e7c4c37fb78d5faa59818e5f43e76
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-08-10 13:42:27 -0700

    add log for new message

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ client.on('guildMemberAdd', (member) => {
       .setDescription(
         `You are now a member of a growing community of Strapi developers/users from all over the world who build awesome projects, and support each other.\n\n👋 We'd **love** to get to know you a bit better, so please **[fill out this short survey](https://strapi.typeform.com/to/TLM3Ae?channel=U01JT604ER2)** then introduce yourself in the [#introduction channel](https://discord.gg/2ZVmbQAuKW)! \n\nIf you're stuck for things to say, here are a few ideas: \n- Where you're from? \n- What tech stack do you use? \n- Your experience with Strapi? \n- What have you been working on?`
       );
+    console.log(`Sending new user message to: ${user.username}`);
     user.send({ embed: newUserEmbed });
   });
 });
