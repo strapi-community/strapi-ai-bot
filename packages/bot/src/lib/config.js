f9b6675c85e9457217bcfa7c411a99565ff3e103
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-11 01:50:55 -0500

    refactor(config): move to lib

--- packages/bot/src/lib/config.js
+++ packages/bot/src/lib/config.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const env = require('./lib/env-helper');
+const { env } = require('./utils/env');
 
 module.exports = {
-  discordToken: env('TOKEN'),
   owners: env.array('OWNERS'),
   backendUrl: env('BACKEND_URL', 'http://localhost:1337'),
 };
