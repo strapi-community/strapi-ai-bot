096e7fcc284ababeb0022edd8015eedb5d3cea62
derrickmehaffy <derrickmehaffy@gmail.com>
2022-03-11 15:25:18 -0700

    feat(backend): init environmental backend configs

--- packages/backend/src/admin/app.example.js
+++ packages/backend/src/admin/app.example.js
@@ -1,33 +1,10 @@
 export default {
   config: {
-    locales: [
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      // 'es',
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      // 'tr',
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
+    locales: [],
+    tutorials: false,
+    notifications: {
+      release: false
+    }
   },
   bootstrap(app) {
     console.log(app);
