c0a02d664a57011c0abbcf3b78ddf6b527bd495d
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-10 20:59:04 -0500

    feat: add auto survey and CTA

--- bot-src/src/index.js
+++ bot-src/src/index.js
@@ -32,20 +32,6 @@ const main = async () => {
 		client.destroy();
 		throw new Error(error)
 	}
-
-	// TODO: old bot code to send survey, we need to move this
-	client.on('guildMemberAdd', (member) => {
-		client.users.fetch(member.id, false).then((user) => {
-			const newUserEmbed = new client.MessageEmbed()
-				.setColor('#00ffff')
-				.setTitle(`🙋‍♀️ Hello ${user.username}`)
-				.setDescription(
-					`You are now a member of a growing community of Strapi developers/users from all over the world who build awesome projects, and support each other.\n\n👋 We'd **love** to get to know you a bit better, so please **[fill out this short survey](https://strapi.typeform.com/to/TLM3Ae?channel=U01JT604ER2)** then introduce yourself in the [#introduction channel](https://discord.gg/2ZVmbQAuKW)! \n\nIf you're stuck for things to say, here are a few ideas: \n- Where you're from? \n- What tech stack do you use? \n- Your experience with Strapi? \n- What have you been working on?`
-				);
-			client.logger.debug(`Sending new user message to: ${user.username}`);
-			user.send({ embed: newUserEmbed });
-		});
-	});
 };
 
 main();
