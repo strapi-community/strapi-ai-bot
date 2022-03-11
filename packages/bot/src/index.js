3f5214523d327d714a45c4477ada7fa8db4aed01
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-11 01:57:06 -0500

    refactor(bot core): use an IIFE for bot initialization
    refactor(bot token): remove need for token to be passed in. by default discord.js uses `DISCORD_TOKEN`

--- packages/bot/src/index.js
+++ packages/bot/src/index.js
@@ -1,38 +1,40 @@
 'use strict';
 
-require('./lib/setup');
 const { LogLevel, SapphireClient } = require('@sapphire/framework');
 const { Intents } = require('discord.js');
-const { discordToken } = require('./config');
+const setup = require('./lib/setup');
 
-const client = new SapphireClient({
-  logger: {
-    level: LogLevel.Debug,
-  },
-  shards: 'auto',
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-  ],
-});
+// IIFE for starting the bot
+(async () => {
+  // Setup discord bot client
+  const client = new SapphireClient({
+    logger: {
+      level: LogLevel.Debug,
+    },
+    shards: 'auto',
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_BANS,
+      Intents.FLAGS.GUILD_MEMBERS,
+      Intents.FLAGS.GUILD_VOICE_STATES,
+      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.DIRECT_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    ],
+  });
 
-const main = async () => {
   try {
-    client.logger.info('Logging in');
-    await client.login(discordToken);
-    client.logger.info('logged in');
+    client.logger.info('Attempting setup initialization.');
+    setup.init();
+    client.logger.info('Setup successfully.');
+    client.logger.info('Attempting to log in.');
+    await client.login();
+    client.logger.info('Logged in successfully.');
   } catch (error) {
     client.logger.fatal(error);
     client.destroy();
     throw new Error(error);
   }
-};
-
-main();
+})();
