cb9ab8b1c66f42761bad530fd7ad8e831702b660
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-20 22:56:39 -0400

    refactor(StrapiAIClient): add a StrapiAI class for bot client options

--- packages/bot/src/index.js
+++ packages/bot/src/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const { LogLevel, SapphireClient } = require('@sapphire/framework');
-const { Intents } = require('discord.js');
 const path = require('path');
 const dotenv = require('dotenv');
+const { StrapiAI } = require('./lib/structures/StrapiAI');
 
 // Setup ENV variables, must be outside function scope
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
@@ -11,30 +10,13 @@ if (dotenv.error) {
   throw new Error(dotenv.error);
 }
 
+// setup must be required after dotenv load, otherwise env variables will not be present
 const setup = require('./lib/setup');
+
+const client = new StrapiAI();
 
 // IIFE for starting the bot
 (async () => {
-  // Setup discord bot client
-  const client = new SapphireClient({
-    logger: {
-      level: LogLevel.Debug,
-    },
-    loadDefaultErrorListeners: false,
-    shards: 'auto',
-    intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_BANS,
-      Intents.FLAGS.GUILD_MEMBERS,
-      Intents.FLAGS.GUILD_VOICE_STATES,
-      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.DIRECT_MESSAGES,
-      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-      Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-    ],
-  });
-
   try {
     client.logger.info('Attempting setup initialization.');
     setup.init();
