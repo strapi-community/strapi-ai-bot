2e898b15235184b534a49af7c07b79017ae59f4f
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-09-15 01:58:25 -0400

    refactor(commands): use slash commands

--- packages/bot/src/commands/faq.js
+++ packages/bot/src/commands/faq.js
@@ -1,35 +1,51 @@
 'use strict';
 
 const { Command } = require('@sapphire/framework');
+const { isMessageInstance } = require('@sapphire/discord.js-utilities');
 const { embedBuilder } = require('../lib/utils/embedBuilder');
 
 class FAQCommand extends Command {
   constructor(context, options) {
-    super(context, {
-      ...options,
-      description: 'Search for a faq',
-    });
+    super(context, { ...options, name: 'faq', description: 'Search for an faq' });
   }
 
-  async messageRun(message, args) {
+  registerApplicationCommands(registry) {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption((option) =>
+          option.setName('title').setDescription('The faq title').setRequired(true)
+        )
+    );
+  }
+
+  async chatInputRun(interaction) {
     const { $api } = this.container;
+    const faqTitle = interaction.options.getString('title');
+    const msg = await interaction.reply({
+      content: `Fetching faq with title ${faqTitle} .....`,
+      ephemeral: true,
+      fetchReply: true,
+    });
+    if (isMessageInstance(msg)) {
+      const faq = await $api.faqs.byTitle(faqTitle);
 
-    const faqTitle = await args.pick('string');
-    const faq = await $api.faqs.byTitle(faqTitle);
-
-    if (!faq) {
-      return message.reply(`A faq with the title \`${faqTitle}\` was not found.`);
+      if (!faq) {
+        return interaction.editReply(`A faq with the title \`${faqTitle}\` was not found.`);
+      }
+      return interaction.editReply({
+        embeds: [
+          embedBuilder({
+            title: faq.attributes.title,
+            description: faq.attributes.content,
+            timestamp: true,
+          }),
+        ],
+      });
     }
 
-    return message.reply({
-      embeds: [
-        embedBuilder({
-          title: faq.attributes.title,
-          description: faq.attributes.content,
-          timestamp: true,
-        }),
-      ],
-    });
+    return interaction.editReply('Only slash commands are supported');
   }
 }
 
--- packages/bot/src/commands/ping.js
+++ packages/bot/src/commands/ping.js
@@ -1,24 +1,27 @@
 'use strict';
 
 const { Command } = require('@sapphire/framework');
-const { send } = require('@sapphire/plugin-editable-commands');
+const { isMessageInstance } = require('@sapphire/discord.js-utilities');
 
 class PingCommand extends Command {
   constructor(context, options) {
-    super(context, {
-      ...options,
-      description: 'ping pong',
-    });
+    super(context, { ...options, name: 'ping', description: 'Ping bot to see if it is alive' });
   }
 
-  async messageRun(message) {
-    const msg = await send(message, 'Ping?');
+  registerApplicationCommands(registry) {
+    registry.registerChatInputCommand((builder) =>
+      builder.setName(this.name).setDescription(this.description)
+    );
+  }
 
-    const content = `Pong from JavaScript! Bot Latency ${Math.round(
-      this.container.client.ws.ping
-    )}ms. API Latency ${msg.createdTimestamp - message.createdTimestamp}ms.`;
-
-    return send(message, content);
+  async chatInputRun(interaction) {
+    const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
+    if (isMessageInstance(msg)) {
+      const diff = msg.createdTimestamp - interaction.createdTimestamp;
+      const ping = Math.round(this.container.client.ws.ping);
+      return interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
+    }
+    return interaction.editReply('Failed to retrieve ping :(');
   }
 }
 
