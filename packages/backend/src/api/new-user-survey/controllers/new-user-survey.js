ca399c5fefab9469267a03df806abc4e11e79cb6
derrickmehaffy <derrickmehaffy@gmail.com>
2023-02-01 14:16:19 -0700

    Start work on survey with Paul

---
+++ packages/backend/src/api/new-user-survey/controllers/new-user-survey.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * new-user-survey controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::new-user-survey.new-user-survey');
---
+++ packages/backend/src/api/new-user-survey/routes/new-user-survey.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * new-user-survey router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::new-user-survey.new-user-survey');
